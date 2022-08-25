7259a1f7884a3077978e054329652b1e7ff3ef70
Helpportel <111671358+Helpportel@users.noreply.github.com>
2022-08-25 14:57:45 +0530

    ass

---
+++ conection.php
@@ -0,0 +1,19 @@
+<?php
+	$servername = "localhost";
+	$username ="root";
+	$pass     = "";
+	$dbname   ="form";
+
+	$conn=mysqli_connect($servername,$username,$pass,$dbname);
+
+	if($conn) {
+		/*echo "conected ok";*/
+	
+	}
+	else{ echo "conection failed";}
+
+
+	
+
+
+?>
